59b9413876a974e3bfced94d0df903d94004de0e
IlyaMenkov <imenkov@mirantis.com>
2016-01-18 16:12:48 +0300

    let's test controller

--- full_mcv_suite.py
+++ full_mcv_suite.py
@@ -29,25 +29,13 @@ simple_opts = [
                 help=('True enables, False disables')),
     cfg.StrOpt('ssh_key_loc', default=False,
                 help=('True enables, False disables'))
+
+    # need add checks for instance_ip
 ]
 
 CONF = cfg.CONF
 CONF.register_group(opt_group)
 CONF.register_opts(simple_opts, opt_group)
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(CONF.basic.controller_ip, username='root', password='r00tme', key_filename='/tmp/id_rsa')
-ssin, ssout, sserr = ssh.exec_command('echo CONF.basic.instance_ip >> logfile.log')
-#print ssout.readlines()
-ssh.close()
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(CONF.basic.instance_ip, username='mcv', password='mcv', key_filename='/tmp/id_rsa')
-hjkin, qqq, hjgjlk = ssh.exec_command('sudo mcvconsoler --run custom resources')
-#print qqq.readlines()
-ssh.close()
 
 if __name__ == "__main__":
     CONF(default_config_files=['mcvbv.conf'])
@@ -63,3 +51,30 @@ if __name__ == "__main__":
     print(CONF.basic.private_endpoint_ip)
     print(CONF.basic.image_url)
     print(CONF.basic.ssh_key_loc)
+
+# work with controller
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(CONF.basic.controller_ip, username='root', password='r00tme', key_filename='/tmp/id_rsa')
+    qqq, www, eee = ssh.exec_command('wget %s &&'
+                                     '. openrc'
+                                     'sudo apt-get install sshpass'
+                                     'sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config'
+                                     'service ssh restart'
+                                     'glance image-create --name mcv --disk-format qcow2 --container-format bare --is-public true --file %s --progress'
+                                     'network_id=`neutron net-list | grep \'net04 \' | awk -F"|" {\'print $2\'} | awk \'{ gsub (" ", "", $0); print}\'`'
+                                     'nova boot --image mcv --flavor m1.large --nic net-id=$network_id mcv_vm'
+                                     'instance_ip=`nova floating-ip-create | grep \'net04\' | awk -F"|" {\'print $3\'} | awk \'{ gsub (" ", "", $0); print}\'`'
+                                     'nova floating-ip-associate mcv_vm %s'
+                                     'nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0'
+                                     'nova secgroup-add-rule default tcp 22 22 0.0.0.0/0' % (str(CONF.basic.image_url), image_name, str(CONF.basic.instance_ip)))
+    print (www.readlines())
+    ssh.close()
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(CONF.basic.instance_ip, username='mcv', password='mcv', key_filename='/tmp/id_rsa')
+    qqq, www, eee = ssh.exec_command('sudo mcvconsoler --run custom resources')
+    print (www.readlines())
+    ssh.close()
+
